8fd69ebd80f75debf9a8037e953f2186a5e5e639
young1112 <99293037+young1112@users.noreply.github.com>
2022-04-08 17:10:07 +0100

    Create Brand.py

---
+++ Brand.py
@@ -0,0 +1,26 @@
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from pc import main_apv
+
+    main_apv()
+
+elif bit == '32bit':
+
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
+
